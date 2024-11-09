95b95116cd99633e44c62c1b3dac6424597980e1
collins-kimotho <collinskimotho16@gmail.com>
2024-11-09 15:35:20 +0300

    create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,7 @@ import re
 import logging
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
@@ -20,6 +21,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         lambda match: match.group().split('=')[0] + f"={redaction}",
         message
     )
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class for obfuscating sensitive information. """
