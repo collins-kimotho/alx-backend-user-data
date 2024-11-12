2cda079537207385153af39f125a207d15d6beab
collins-kimotho <collinskimotho16@gmail.com>
2024-11-12 20:33:35 +0300

    basic user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -52,3 +52,27 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts user email and password from the Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header: The decoded Base64 string.
+
+        Returns:
+            tuple: (user_email, user_password) if the input is valid.
+            (None, None): if the input is invalid or doesn't contain a colon.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        # Split the string on the first occurrence of ':'
+        user_email, user_password = decoded_base64_authorization_header.split(
+            ":", 1)
+        return user_email, user_password
