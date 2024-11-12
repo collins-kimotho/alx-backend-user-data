d344711ff9ceeabc0edd8267a48b8cd9a5e35b24
collins-kimotho <collinskimotho16@gmail.com>
2024-11-12 14:14:11 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,10 +16,32 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required for a given path.
+        Args:
+            path (str): The path to check.
+            excluded_paths: A list of paths that don't require authentication.
+        Returns:
+            bool: False for now.
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Returns the authorization header from the request.
+        Args:
+            request: The Flask request object.
+        Returns:
+            str: None, as authorization header extraction is not implemented.
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user from the request.
+        Args:
+            request: The Flask request object.
+        Returns:
+            TypeVar('User'): None, as user extraction is not implemented.
+        """
         return None
