b8e93d03daf3dd2b8df14fe9276cee2281bc5ade
collins-kimotho <collinskimotho16@gmail.com>
2024-11-12 14:48:46 +0300

    request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,14 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth  # Import Auth class
 
+
+# Initialize `auth` based on `AUTH_TYPE`
+auth = None
+auth_type = getenv("AUTH_TYPE")
+if auth_type == "auth":
+    auth = Auth()
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
