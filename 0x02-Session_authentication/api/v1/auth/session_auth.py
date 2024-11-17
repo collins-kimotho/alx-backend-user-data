a4bdbbd26f049ac34609e08cc465bc5c387cb379
collins-kimotho <collinskimotho16@gmail.com>
2024-11-17 12:24:42 +0300

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Session authentication module
+"""
+
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Implements session based authentication
+    """
+    pass
