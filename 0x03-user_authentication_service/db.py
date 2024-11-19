ed67e2ec9246b492fd1ab9109354d6b3987e569b
collins-kimotho <collinskimotho16@gmail.com>
2024-11-19 13:02:55 +0300

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -69,15 +69,28 @@ class DB:
             NoResultFound: If no user matches the criteria.
             InvalidRequestError: If the query arguments are invalid.
         """
-        if not kwargs:
-            raise InvalidRequestError("No query arguments provided")
+        # if not kwargs:
+        #     raise InvalidRequestError("No query arguments provided")
 
-        # Query the database with the provided filters
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if not user:
-                raise NoResultFound("No user found matching the criteria")
-            return user
-        except AttributeError as e:
-            # Raised when the query uses invalid attributes
-            raise InvalidRequestError(f"Invalid query argument: {e}")
+        # # Query the database with the provided filters
+        # try:
+        #     user = self._session.query(User).filter_by(**kwargs).first()
+        #     if not user:
+        #         raise NoResultFound("No user found matching the criteria")
+        #     return user
+        # except AttributeError as e:
+        #     # Raised when the query uses invalid attributes
+        #     raise InvalidRequestError(f"Invalid query argument: {e}")
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
