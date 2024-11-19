d3a46969fc113c3dc085b38b5922d4cf86d3edaf
collins-kimotho <collinskimotho16@gmail.com>
2024-11-19 12:31:04 +0300

    create user

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Add a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The created user object.
+        """
+        # Create a new User instance
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        # Add the new user to the session
+        self._session.add(new_user)
+
+        # Commit the transaction to persist changes to the database
+        self._session.commit()
+
+        # Return the newly created User object
+        return new_user
