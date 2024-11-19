22e94fcd9e936e61204d654158da7936c15a895f
collins-kimotho <collinskimotho16@gmail.com>
2024-11-19 12:38:28 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""DB module
 """
+DB module
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,21 +12,18 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class for managing the database."""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """Initialize a new DB instance."""
         self._engine = create_engine("sqlite:///a.db", echo=True)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
+        Base.metadata.drop_all(self._engine)  # Drop all tables (testing)
+        Base.metadata.create_all(self._engine)  # Create all tables
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -41,22 +40,14 @@ class DB:
         Returns:
             User: The created user object.
         """
-        # # Create a new User instance
-        # new_user = User(email=email, hashed_password=hashed_password)
+        # Create a new User instance
+        new_user = User(email=email, hashed_password=hashed_password)
 
-        # # Add the new user to the session
-        # self._session.add(new_user)
+        # Add the new user to the session
+        self._session.add(new_user)
 
-        # # Commit the transaction to persist changes to the database
-        # self._session.commit()
+        # Commit the transaction to persist changes to the database
+        self._session.commit()
 
-        # # Return the newly created User object
-        # return new_user
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
+        # Return the newly created User object
         return new_user
