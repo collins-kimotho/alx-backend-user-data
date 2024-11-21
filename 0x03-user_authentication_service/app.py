8ed36ccbec14bfd8ae7b98f22b7331951c7d1013
collins-kimotho <collinskimotho16@gmail.com>
2024-11-21 08:30:13 +0300

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,10 +3,12 @@
 Flask app that returns a simple JSON response.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 # Initialize the Flask app
 app = Flask(__name__)
+AUTH = Auth()
 
 
 #  Define the route for the home page
@@ -14,6 +16,35 @@ app = Flask(__name__)
 def home():
     """Return a simple JSON response."""
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """
+    POST /users endpoint for user registration
+
+    Expects form data: 
+        - email: The user's email
+        - password: The user's password
+    Returns:
+        - On SUccess: JSON payload
+        - On failure: JSON payload and 400 status code
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({
+            "message": "email and password are requred"
+        }), 400
+    
+    try:
+        # Register the user
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        # Handle duplicate email registration
+        return jsonify({"message": "email already registered"}), 400
+
 
 
 # Run the app when the script is executed
