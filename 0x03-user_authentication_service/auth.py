34c9abddc61200eb1d560cd43d7da3ac69a8a166
collins-kimotho <collinskimotho16@gmail.com>
2024-11-21 08:50:31 +0300

    get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -58,19 +58,36 @@ class Auth:
             return False
         return False
 
+    # def create_session(self, email: str) -> str:
+    #     """Creates a new session for a user.
+    #     """
+    #     user = None
+    #     try:
+    #         user = self._db.find_user_by(email=email)
+    #     except NoResultFound:
+    #         return None
+    #     if user is None:
+    #         return None
+    #     session_id = _generate_uuid()
+    #     self._db.update_user(user.id, session_id=session_id)
+    #     return session_id
     def create_session(self, email: str) -> str:
-        """Creates a new session for a user.
         """
-        user = None
+        Create a session ID for a user.
+
+        Args:
+            email (str): The user's email.
+
+        Returns:
+            str: The session ID.
+        """
         try:
             user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return None
-        if user is None:
-            return None
-        session_id = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Retrieves a user based on a given session ID.
